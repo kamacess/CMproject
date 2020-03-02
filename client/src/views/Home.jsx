6886b9544833309c79ec0ac3553346798289585c
Cess Flech <cecile.flechon@gmail.com>
2020-03-02 17:45:36 +0100

    styling

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -4,6 +4,8 @@ import io from 'socket.io-client';
 const ioClient = io.connect('http://localhost:4000');
 // import { Link } from 'react-router-dom';
 
+import './../styles/style.css';
+
 export default function Home() {
 
 	const socket = io('http://127.0.0.1:4000');
@@ -15,10 +17,27 @@ export default function Home() {
 
     return (
         <div className="home">
-            <h1>Le nom du biniou</h1>
-            <img src="./../../assets/yellow-cat.png" alt="the yellow cat smiles at you"/>
-            <ListActionBack socket={socket}/>
+            <main>
+        TON COMPOSANT REACT MAIN
+
+        <ListActionBack socket={socket}/>
             <button onClick={sendMessage}>pr00t2THE M4XXXX</button>
+    </main>
+
+    <aside>
+        <h2>was passiert???</h2>
+    
+        <ul>
+            <li> emotional alligator has voted </li>
+            <li>medium crab has voted</li>
+            <li>necessary zebra has prouted</li>
+            <li>ambitious mongoose has voted</li>
+            <li>busy hamster has voted</li>
+            <li>bewitched pig has prouted</li>
+            <li>supportive bee has voted</li>
+        </ul>
+    </aside>
+            
         </div>
     )
 }
