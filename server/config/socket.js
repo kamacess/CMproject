0da778f2baad1461e9ee42b21a8ef2f611cc53cc
Anthony Leduc <aleduc88@gmail.com>
2020-03-02 18:01:22 +0100

    Anthony V1

--- server/config/socket.js
+++ server/config/socket.js
@@ -1,7 +1,26 @@
 module.exports = function(socket){
     console.log("ici")
+    
+    
     socket.on('send-message', function(message){
       socket.broadcast.emit('message', message)
     //   socket.emit('message', message)
+
+
+    // for (var pickedMedias=[],i=0;i<5;++i) pickedMedias[i]=i;
+     
+    //   var media = {
+    //     type: 'video, text, image, audio, music, pdf'
+    // }
+    //     console.log("ici")
+    //     const randomMedia = pickedMedias[Math.floor(Math.random()*media.type)]
+    //     socket.on('send-media', function(media){
+    //     var users = {};
+    //     users[USER_NAME] = socket.id;
+    //     io.sockets.connected[users[USER_NAME]]
+    //     .emit('private', randomMedia);
+    //     //   socket.emit('message', message)
+    //     });
     });
 }
+
